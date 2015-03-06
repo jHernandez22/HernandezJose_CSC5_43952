1f03174b790882141036d1f047a4a078a2eca2ba
jHernandez <izanagi022@gmail.com>
2015-03-06 13:03:05 -0800

    Signed-off-by: jHernandez <izanagi022@gmail.com>

--- HmWk/Assignment 1/Savitch_9thEd_Chap1_Prob1/nbproject/Makefile-impl.mk
+++ HmWk/Assignment 1/Savitch_9thEd_Chap1_Prob1/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Savitch_9thEd_Chcap1_Prob1
+PROJECTNAME=Savitch_9thEd_Chap1_Prob1
 
 # Active Configuration
 DEFAULTCONF=Debug
--- HmWk/Assignment 1/Savitch_9thEd_Chap1_Prob5/main.cpp
+++ HmWk/Assignment 1/Savitch_9thEd_Chap1_Prob5/main.cpp
@@ -7,6 +7,7 @@
 
 //System Libraries
 #include <iostream>//I/O Library
+#include <cmath>//I/O Library
 using namespace std; //I/O Library Under std namespace
 
 //User Libraries
@@ -18,16 +19,16 @@ using namespace std; //I/O Library Under std namespace
 //Execution Start Here!
 int main(int argc, char** argv) {
     //Define Variables
-    int fncWth,fncLnth,totPrm;
+    int fncWth,fncLnth,totPrim;
     cout<<"Press return after entering a number."<<endl;
-    cout<<"Enter the number of pods:"<<endl;
-    cin>>numPods;
-    cout<<"Enter the number of peas in a pod:"<<endl;
-    cin>>psPod;
-    totPrm=(fncWth*2)+(fncLnth*2);
-    cout<<"If you have "<<numPods<<" pea pods"<<endl;
-    cout<<"and "<<psPod<<" peas in each pod, then"<<endl;
-    cout<<"you have "<<totPs<<" peas in all the pods."<<endl;
+    cout<<"Enter the Width of Fence:"<<endl;
+    cin>>fncLnth;
+    cout<<"Enter the Length of Fence:"<<endl;
+    cin>>fncWth;
+    totPrim=(fncWth*2)+(fncLnth*2);
+    cout<<"If you have a length of "<<fncLnth<<endl;
+    cout<<"and a with of "<<fncWth<<", then"<<endl;
+    cout<<"the total perimeter of the rectangle is "<<totPrim<<"."<<endl;
     
     //Exit Stage Right
     return 0;
