0506364c6a812e7940a2fdb12e5dcad6ef8ed2c8
Izanagi22 <jahsystem22@yahoo.com>
2015-03-09 00:10:26 -0700

    Signed-off-by: Izanagi22 <jahsystem22@yahoo.com>

--- HmWk/Assignment 1/Savitch_9thEd_Chap1_ProgProj_Prob3/main.cpp
+++ HmWk/Assignment 1/Savitch_9thEd_Chap1_ProgProj_Prob3/main.cpp
@@ -19,8 +19,22 @@ using namespace std; //I/O Library Under std namespace
 
 int main(int argc, char** argv) {
     //Declare Variables
-    
-
+    float numQtrs,NumDims,numNkls,totSum;
+   
+    //Input Numbers
+    cout<<"Press return after entering a Number"<<endl;
+    cout<<"Enter the number of Quarters."<<endl;
+    cin>>numQtrs;
+    cout<<"Enter the number of Dimes."<<endl;
+    cin>>NumDims;
+    cout<<"Enter the number of Nickels."<<endl;
+    cin>>numNkls;
+   
+    //Calculate the Total Sum in Cents
+    totSum=numQtrs*25+NumDims*10+numNkls*5;
+    cout<<"The Total Sum of "<<numQtrs<<" Quarters, "<<NumDims<<" Dimes, and ";
+    cout<<numNkls<<" Nickels is "<<totSum<<" cents."<<endl;
+   
     //Exit Stage Right!
     return 0;
 }
