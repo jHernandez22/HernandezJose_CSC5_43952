45b7b51a88329fcbad942fed5432a5c9f3711223
jHernandez <izanagi022@gmail.com>
2015-03-08 19:33:13 -0700

    Signed-off-by: jHernandez <izanagi022@gmail.com>

--- HmWk/Assignment 1/Savitch_9thEd_Chap1_ProgProj_Prob1/main.cpp
+++ HmWk/Assignment 1/Savitch_9thEd_Chap1_ProgProj_Prob1/main.cpp
@@ -18,8 +18,17 @@ using namespace std; //I/O Library Under std namespace
 //Execution Start Here!
 int main(int argc, char** argv) {
     //Declare Variables
-
-
+     int numPods,psPod,sum,product;
+    cout<<"Press return after entering a number."<<endl;
+    cout<<"Enter the number of pods and number of peas in a pod:"<<endl;
+    cin>>numPods>>psPod;
+    //cout<<"Enter the number of peas in a pod:"<<endl;
+    //cin>>psPod;
+    sum=numPods+psPod;
+    cout<<"If you have "<<numPods<<" pea pods"<<endl;
+    cout<<"and "<<psPod<<" peas in each pod, then"<<endl;
+    cout<<"you have "<<totPs<<" peas in all the pods."<<endl;
+    cout<<"This is the end of the program"<<endl;
     //Exit Stage Right!
     return 0;
 }
